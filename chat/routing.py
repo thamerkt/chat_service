b6c03b103a4479e55ff3c9c98e13d9cc45fe3094
thamerkt <kthirithamer1@gmail.com>
2025-06-27 17:05:34 +0100

    Update routing.py

--- chat/routing.py
+++ chat/routing.py
@@ -11,9 +11,11 @@ UUID_PATTERN = (
     r'[0-9a-fA-F]{12}'
 )
 
+USER_ID_PATTERN = rf'(?:{UUID_PATTERN}|\d+)'  # Accepts either UUID or numeric IDs
+
 websocket_urlpatterns = [
     re_path(
-        rf'^ws/chatroom/(?P<room_id>[\w\-]+(?:_[\w\-]+)*)/(?P<user_id>{UUID_PATTERN})/$',
+        rf'^ws/chatroom/(?P<room_id>[\w\-]+(?:_[\w\-]+)*)/(?P<user_id>{USER_ID_PATTERN})/$',
         consumers.ChatConsumer.as_asgi()
     ),
     re_path(
